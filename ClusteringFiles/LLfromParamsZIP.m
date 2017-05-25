115e528bcf01ef75237728efa3f801f8fa2e8563
Sumit Mukherjee <sumitmukherjee2@gmail.com>
2017-05-25 12:44:53 -0700

    Updating ZIP

--- ClusteringFiles/LLfromParamsZIP.m
+++ ClusteringFiles/LLfromParamsZIP.m
@@ -9,8 +9,8 @@ I = find(x>=1);
 
 temp = zeros(r,c); 
 
-temp(I) = +log(1 - M(I)) + x(I).*log(L(I)) - L(I); 
-temp(I2) = +log(M(I2) + (1-M(I2)).*exp(-L(I2))) - (x(I2).*log(x(I2)) - x(I2) + .5*log(2*pi*x(I2))); 
+temp(I) = +log(1 - M(I)) + x(I).*log(L(I)) - L(I) - (x(I).*log(x(I)) - x(I) + .5*log(2*pi*x(I))); 
+temp(I2) = +log(M(I2) + (1-M(I2)).*exp(-L(I2))) ; 
 
 if r>1
     LogLike = sum(temp); 
